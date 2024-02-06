c23bc9cf171c02718649fde8cb6059ae16dce25a
ali mandela <muhammadalimandela01@gmail.com>
2024-02-06 23:13:43 +0530

    inital

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "amlif3ali": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
